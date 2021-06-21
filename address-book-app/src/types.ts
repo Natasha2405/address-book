fc45a30e636084289da1148ab72ff8e2b7ab7faa
Natasha <natasa.gajik@gmail.com>
2021-06-21 17:09:55 +0200

    types

--- address-book-app/src/types.ts
+++ address-book-app/src/types.ts
@@ -1,1 +1,20 @@
-export {}
+export interface Contact {
+	id: string,
+	first_name: string,
+	last_name: string,
+	email: string,
+	country: string
+};
+export interface ContactsState {
+	contacts: Contact[]
+};
+
+export interface AplicationState {
+	contacts: ContactsState
+}
+
+export interface ContactsListActions {
+	type: string,
+	payload: Contact,
+	contacts: Contact[]
+};
