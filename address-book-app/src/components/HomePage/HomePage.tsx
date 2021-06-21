cad7b87d3d3d1450411f20cb9789dc272f4be790
Natasha <natasa.gajik@gmail.com>
2021-06-21 17:09:16 +0200

    basics on the home page

--- address-book-app/src/components/HomePage/HomePage.tsx
+++ address-book-app/src/components/HomePage/HomePage.tsx
@@ -1,16 +1,31 @@
 // vendor imports
 import React from 'react';
+import { useSelector } from 'react-redux';
 
+import { Contact, AplicationState } from '../../types';
+
+// import { setContacts } from '../../redux/ducks/contacts';
 // styles
 import './style.css';
 
-export const HomePage: React.FC = () => {
-	
+interface StateProps {
+	contacts: Contact[],
+}
+
+export const HomePage: React.FC<StateProps> = () => {
+
+	let data = useSelector((state: AplicationState) => state.contacts);
+	let contacts = data['contacts'];
+	console.log(contacts);
+
 
 	return (
 		<div className="homepage-container">
-            <h2>Your Contacts:</h2>
-			
+			<h2>Your Contacts:</h2>
+			{contacts.map(contact => (
+				<h2>{contact.first_name}</h2>
+			))}
+
 		</div>
 	);
 };
