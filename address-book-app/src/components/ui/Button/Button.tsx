827fa37a2e47331598cf3b9cfabd5667ee823b8f
Natasha <natasa.gajik@gmail.com>
2021-06-21 17:18:23 +0200

    ui-components: button and input

--- address-book-app/src/components/ui/Button/Button.tsx
+++ address-book-app/src/components/ui/Button/Button.tsx
@@ -1,1 +1,20 @@
-export{}
+// vendor imports
+import React from 'react';
+
+// style
+import './style.css';
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+	label: string,
+};
+
+export const Button: React.FC<ButtonProps> = ({ label, ...props }) => {
+	return (
+		<button
+			className='active-button'
+			type={props.type}
+		>
+			{label}
+		</button>
+	);
+};
--- address-book-app/src/components/ui/Input/Input.tsx
+++ address-book-app/src/components/ui/Input/Input.tsx
@@ -1,1 +1,27 @@
-export {}
+// vendor imports
+import React from 'react';
+
+// style
+import './style.css';
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+	label: string,
+	name: string
+}
+
+export const Input: React.FC<InputProps> = ({ label, name, ...props }) => {
+	return (
+		<div className='input-field-wrapper'>
+			<span className='input-field__label'>{label}</span>
+			<br />
+			<input
+				className='input-field'
+				name={name}
+				placeholder={props.placeholder}
+				type={props.type}
+				value={props.value}
+				onChange={props.onChange}
+			/>
+		</div>
+	);
+};
