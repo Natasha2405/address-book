682259b5ea0d1aeb17637dd2eba4a7ee5d2ce133
Natasha <natasa.gajik@gmail.com>
2021-06-21 17:19:07 +0200

    setting basics on new-contact-form

--- address-book-app/src/components/widgets/NewContactForm/NewContactForm.tsx
+++ address-book-app/src/components/widgets/NewContactForm/NewContactForm.tsx
@@ -1,1 +1,18 @@
-export {}
+// vendor imports
+import React from 'react';
+// components
+import { Input } from '../../ui/Input/Input';
+import { Button } from '../../ui/Button/Button';
+// validation
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+// style
+import './style.css';
+
+export const NewContactForm = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
