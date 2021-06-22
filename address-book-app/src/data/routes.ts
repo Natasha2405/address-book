6dca802b0b389e60dc8cd1ee6227b522f7e2534d
Natasha <natasa.gajik@gmail.com>
2021-06-22 18:21:14 +0200

    routes - constants

--- address-book-app/src/data/routes.ts
+++ address-book-app/src/data/routes.ts
@@ -1,1 +1,11 @@
-export {}
+const ROOT = '/';
+const ADD_NEW_CONTACT = '/contacts/new-contact';
+const EDIT_CONTACT = '/contacts/update-contact/:id';
+
+let routes = {
+    ROOT,
+    ADD_NEW_CONTACT,
+    EDIT_CONTACT
+}
+
+export default routes;
